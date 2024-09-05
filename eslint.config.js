af3a85dff2a959ddb2e19021e4536f38aa6a7b04
Dai Xuezhou <daixuezhou@bytedance.com>
2024-09-06 00:42:56 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default antfu({
+  formatters: true,
+  unocss: true,
+}, ...compat.config({
+  overrides: [
+    {
+      files: [
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/support/**/*.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:cypress/recommended'
+      ]
+    }
+  ]
+}))
---
+++ uno.config.ts
@@ -0,0 +1,24 @@
+import {
+    defineConfig,
+    presetIcons,
+    presetUno,
+    transformerDirectives,
+    transformerVariantGroup
+  } from 'unocss'
+  
+  export default defineConfig({
+    shortcuts: [
+    ],
+    theme: {
+      colors: {
+      }
+    },
+    presets: [
+      presetUno(),
+      presetIcons(),
+    ],
+    transformers: [
+      transformerDirectives(),
+      transformerVariantGroup(),
+    ],
+  })
